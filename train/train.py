71c213d524f8ca2e391df98fad1ea42444316e28
yannick lecroart <yannick.lecroart@atos.net>
2021-06-19 00:23:18 +0200

    edited volume mount docker run

--- train/train.py
+++ train/train.py
@@ -17,8 +17,8 @@ def train_model(x_train, y_train):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    
     parser.add_argument('--x_train')
     parser.add_argument('--y_train')
+    
     args = parser.parse_args()
     train_model(args.x_train, args.y_train)
